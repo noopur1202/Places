80333fec8beffb80551a6f78e65c8e32d29756da
nitin <nicky.gurbani@gmail.com>
2018-08-13 10:07:51 +0530

    initial commit

---
+++ Places/app/src/main/java/com/workstation/places/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.workstation.places;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MapActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
